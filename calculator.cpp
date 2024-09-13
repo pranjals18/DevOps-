56fa2973216b3135fed752e932501f67dc7a7f61
Pranjal Shinde <shindept2003@gmail.com>
2024-09-13 16:51:22 +0530

    Added Calculator code

---
+++ calculator.cpp
@@ -0,0 +1,34 @@
+// Create a simple calculaotr app with switch case
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num1, num2, result;
+    char op;
+    cout << "Enter two numbers: ";
+    cin >> num1 >> num2;
+    cout << "Enter an operator: ";
+    cin >> op;
+    switch (op)
+    {
+    case '+':
+        result = num1 + num2;
+        break;
+    case '-':
+        result = num1 - num2;
+        break;
+    case '*':
+        result = num1 * num2;
+        break;  
+    case '/':   
+        result = num1 / num2;
+        break;
+    default:
+        cout << "Invalid operator";
+    }
+    cout << "Result: " << result;
+    return 0;
+}
